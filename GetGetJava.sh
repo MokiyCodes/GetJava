56c8bbab2a8a81d17d8624cd078261aa8e881974
Mokiy <92001109+MokiyCodes@users.noreply.github.com>
2022-01-27 08:29:53 +0100

    Update GetGetJava.sh

--- GetGetJava.sh
+++ GetGetJava.sh
@@ -3,7 +3,9 @@
 # For copy-pasting use in other shell scripts
 if [ ! -d "JavaDL" ]; then
   echo "Downloading GetJava..."
-  wget -qO- https://github.com/MokiyCodes/GetJava/archive/refs/heads/main.zip | bsdtar -xvf- -C .
+  # wget -qO- https://github.com/MokiyCodes/GetJava/archive/refs/heads/main.zip | bsdtar -xvf- -C .
+  curl -#L https://github.com/MokiyCodes/GetJava/archive/refs/heads/main.zip | bsdtar -xf- -C .
+  echo "Moving GetJava-main to JavaDL..."
   mv GetJava-main JavaDL
   echo "Adding Execute Permissions to GetJava..."
   chmod +x JavaDL --recursive
